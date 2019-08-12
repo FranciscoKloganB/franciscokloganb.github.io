25775f2ce30f547c7392b5074816fb82d208d5ca
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-08-13 00:38:36 +0100

    Go

---
+++ README.md
@@ -0,0 +1,7 @@
+# francisco_barros_curriculum_vitae:website
+
+I no longer use this website to promote my curriculum vitae ever since I ignored GDPR e-mails from the free webhost I was using which eventually led to the shutdown of my domain.
+
+This website still had somethings to be done, including, double language presentation and do better animations for my skillset on mobile
+
+I did it mainly to practise HTML5, CSS3 and JavaScript/jQuery after finishing the semester where I first learned this technologies.
---
+++ scripts/landingPage.js
@@ -0,0 +1,105 @@
+const MAX_RATING = 5;
+
+/** Each skill is an array with three elements: name, percent of knowledge and decorating color */
+
+/** Professional Skills */
+const python = ['Python', '80%', 'steelblue'];
+const java = ['Java', '80%', 'firebrick'];
+const csharp = ['C#', '80%', 'darkblue'];
+const cplusplus = ['C++', '60%', 'darkcyan'];
+const c = ['C', '40%', 'cadetblue'];
+const html = ['HTML5', '60%', 'darkorange'];
+const css = ['CSS3', '60%', 'dodgerblue'];
+const javascript = ['JavaScript', '60%', 'gold'];
+
+/** Personal Skills */
+const details = ['Determined', '100%', '#BDC3C7'];
+const responsible = ['Responsible', '80%', '#BDC3C7'];
+const teamplayer = ['Teamplayer', '60%', '#BDC3C7'];
+const competitive = ['Competitive', '60%', '#BDC3C7'];
+
+/** main code */
+var professionalSkills = [python, java, csharp, cplusplus, c, html, css, javascript];
+var personalSkills = [details, responsible, teamplayer, competitive];
+
+$(document).ready(function() {
+  var professional = $('#professionalSkills');
+  var personal = $('#personalSkills');
+  $('.container').fadeIn(2000);
+  for (var i = 0, len = professionalSkills.length; i < len; i++) {
+    newDataPercent(professional, professionalSkills[i]);
+  }
+  for (var i = 0, len = personalSkills.length; i < len; i++) {
+    newDataPercent(personal, personalSkills[i]);
+  }
+  $('head').append('<link rel="stylesheet" type="text/css" href="css/skillBars.css">');
+  fillBars();
+});
+
+/** Helper skill bars methods */
+function newDataPercent(selector, skill) {
+  var title = skill[0];
+  var knowledge = skill[1];
+  var color = skill[2];
+  var newNode = `
+                  <div class="skillbar clearfix" data-percent="` + knowledge + `">
+                    <div class="skillbar-title" style="background: ` + color + `;"><span>` + title + `</span></div>
+                    <div class="skillbar-bar" style="background:` + color + `;"></div>
+                    <div class="skill-bar-percent">` + knowledge + `</div>
+                  </div>
+                  `;
+  selector.append(newNode);
+}
+
+function fillBars() {
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		}, 3000);
+	});
+}
+
+/** event handling */
+$('#gitHubLogo').click(function() {
+  var tab = window.open('https://github.com/FranciscoKloganB', '_blank');
+  if (tab) {
+    tab.focus();
+  } else {
+    swal({
+      title: 'Ups!',
+      text: 'To open my GitHub link you must allow pop-ups for my website.',
+      confirmButtonText: 'OK',
+      width: '40%',
+      confirmButtonClass: 'swalOkButton',
+    })
+  }
+});
+
+$('#istMeicProgram').click(function() {
+  window.open("https://fenix.tecnico.ulisboa.pt/cursos/meic-t/curriculo", '_blank');
+});
+
+$('#istMeicCer').css({"color" : "grey"});
+/* $('#istCer').click(function() {
+  // TODO
+}); */
+
+$('#istLeicProgram').click(function() {
+  window.open("https://fenix.tecnico.ulisboa.pt/cursos/leic-t/curriculo", '_blank');
+});
+
+$('#istLeicCer').click(function() {
+  window.open("docs/ist_leic_diploma.pdf", "Cookery and Food Production graduation certificate", '_blank');
+});
+
+$('#eshteProgram').click(function() {
+  window.open("http://www.eshte.pt/contents/ds_documents/par-7287.pdf", '_blank');
+});
+
+$('#eshteCer').click(function() {
+  window.open("docs/eshte_certificate.pdf", "Cookery and Food Production graduation certificate", '_blank');
+});
+
+$('#eslfbCer').click(function() {
+  window.open("docs/eslfb_diploma.pdf", "High School Diploma", '_blank');
+});
