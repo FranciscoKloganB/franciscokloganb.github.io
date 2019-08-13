315e2e62fe9eba280aa2d4b477e1b9742912cda7
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-08-13 01:45:13 +0100

    Update landingPage.js

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -3,20 +3,20 @@ const MAX_RATING = 5;
 /** Each skill is an array with three elements: name, percent of knowledge and decorating color */
 
 /** Professional Skills */
-const python = ['Python', '80%', 'steelblue'];
-const java = ['Java', '80%', 'firebrick'];
-const csharp = ['C#', '80%', 'darkblue'];
-const cplusplus = ['C++', '60%', 'darkcyan'];
-const c = ['C', '40%', 'cadetblue'];
-const html = ['HTML5', '60%', 'darkorange'];
-const css = ['CSS3', '60%', 'dodgerblue'];
-const javascript = ['JavaScript', '60%', 'gold'];
+const python = ['Python', '80%', '#092e20'];
+const java = ['Java', '80%', '#e89a13'];
+const csharp = ['C#', '80%', '#6a468a'];
+const cplusplus = ['C++', '60%', '#799aba'];
+const c = ['C', '40%', '#00112c'];
+const html = ['HTML5', '60%', '#e34f26'];
+const css = ['CSS3', '60%', '#002561'];
+const javascript = ['JavaScript', '60%', '#f7df1e'];
 
 /** Personal Skills */
-const details = ['Determined', '100%', '#BDC3C7'];
-const responsible = ['Responsible', '80%', '#BDC3C7'];
-const teamplayer = ['Teamplayer', '60%', '#BDC3C7'];
-const competitive = ['Competitive', '60%', '#BDC3C7'];
+const details = ['Determined', '100%', '#bdc3c7'];
+const responsible = ['Responsible', '80%', '#bdc3c7'];
+const teamplayer = ['Teamplayer', '60%', '#bdc3c7'];
+const competitive = ['Competitive', '60%', '#bdc3c7'];
 
 /** main code */
 var professionalSkills = [python, java, csharp, cplusplus, c, html, css, javascript];
